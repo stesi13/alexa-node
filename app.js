cb2fd2936321f817f02f7493c2995dff19b08c80
Stefan Simko <stefan.simko@smilingwords.eu>
2020-05-30 00:29:29 +0200

    bla5

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const PORT = process.env.PORT || 3000;
 // create express app
 const app = express();
 
@@ -16,6 +16,6 @@ app.get('/smw', (req, res) => {
 });
 
 // listen for requests
-app.listen(3000, () => {
+app.listen(PORT, () => {
     console.log("Server is listening on port 3000");
 });
